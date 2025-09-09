0edda357d04cec6ff15b8f6683e6e996bc026fcd
devMirna <mirnafranco58@gmail.com>
2025-09-09 14:46:07 -0300

    se termino Adapter y Obsever

--- src/Factory/Clases/SmsNotifier.ts
+++ src/Factory/Clases/SmsNotifier.ts
@@ -1,10 +1,1 @@
-import { INotifier } from "../Interfaces/INotifier";
 
-export class SmsNotifier implements INotifier {
-  send(to: string, message: string): void {
-    if (!/^\+?\d{7,15}$/.test(to)) {
-      throw new Error(" Número de teléfono inválido");
-    }
-    console.log(` SMS enviado a ${to}: ${message}`);
-  }
-}
