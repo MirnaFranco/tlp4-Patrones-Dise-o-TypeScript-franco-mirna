dfa80b3713430d88b1377960155a581db1687629
devMirna <mirnafranco58@gmail.com>
2025-09-10 09:24:37 -0300

    se termino Factory y singleton

--- src/Factory/Clases/WhatsappNotifier.ts
+++ src/Factory/Clases/WhatsappNotifier.ts
@@ -1,1 +1,10 @@
+import { INotifier } from "../Interfaces/INotifier";
 
+export class WhatsappNotifier implements INotifier {
+  send(to: string, message: string): void {
+    if (!/^\+?\d{7,15}$/.test(to)) {
+      throw new Error(" Número de WhatsApp inválido");
+    }
+    console.log(` WhatsApp enviado a ${to}: ${message}`);
+  }
+}
